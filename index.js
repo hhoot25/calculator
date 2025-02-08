205c9712e75d8374d5d9838e0e95d74ffe43adf7
hhoot25 <hhoot25@gmail.com>
2025-02-07 23:24:16 -0600

    added 4 function operations

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
-let a;
-let b;
-let operator;
+let a = "";
+let b= "";
+let op_bool = false;
+let op = "";
+const display = document.getElementById("display");
+
+
+
+
+const num_elements = document.querySelectorAll('.number');
+
+num_elements.forEach(element => {element.addEventListener('click',() => {
+    
+    
+    if(!op_bool){
+        a += element.id;
+        display.textContent = a;
+    }
+    else{
+        b += element.id;
+        display.textContent = b;
+    }
+    });
+        });
+
+const op_elements = document.querySelectorAll('.operator');
+
+op_elements.forEach(element => {element.addEventListener('click',() => {
+    if(element.id == 'equal' && op_bool == true){
+        display.textContent = operate(parseInt(a),parseInt(b),op);
+        a = operate(parseInt(a),parseInt(b),op);
+        b = "";
+        op = "";
+        op_bool = false;
+        return;
+
+    }
+    if(op_bool == true){
+        document.querySelector('#equal').click();
+    }
+    op_bool = true;
+    op = element.id;
+
+    display.textContent = "";
+
+    });
+        });
+
+const clear = document.querySelector('#clear');
+
+clear.addEventListener('click',() => {
+    a = "";
+    b= "";
+    op_bool = false;
+    op = "";
+    display.textContent = "";
+});
+
+function numClicked(num){
+    
+}
+
 
 function add(a, b){
     return a + b;
@@ -38,3 +97,5 @@ function operate(a,b, op){
             return 0;
     }
 }
+
+
